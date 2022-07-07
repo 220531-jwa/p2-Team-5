39fb96bd1140671277801eab0835f134da10dc83
TCPrater <prater207@revature.net>
2022-07-07 16:13:38 -0500

    Fixing typo in file name

--- p2-Team-5/src/main/java/dev/stenope/models/PetType.java
+++ p2-Team-5/src/main/java/dev/stenope/models/PetType.java
@@ -1,7 +1,18 @@
 package dev.stenope.models;
 
 public class PetType {
-
-	private String sName; //Species Name
-	private String sSRC; //Species Source
+	public static String preloaded[][] = { //row 1 is sName, row 2 is sSRC
+			{"cockroach", "spider", "butterfly", "ladybug",
+			"earthworm", "octopus", "snail", "velvet worm", 
+			"dog", "cat", "rat", "horse", 
+			"owl", "t-rex", "frog", "fish"}, 
+			{"🪳", "🕷️", "🦋", "🐞",
+			"🪱", "🐙", "🐌", "🐛", 
+			"🐕", "🐈", "🐀", "🐎",
+			"🦉", "🦖", "🐸", "🐟"}
+			};
+	
+	private int id;
+	private String sName;
+	private String sSRC;
 }
