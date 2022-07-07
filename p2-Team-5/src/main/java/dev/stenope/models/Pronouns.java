0e6f8b6250038b98f3685c523c3409855f711590
TCPrater <prater207@revature.net>
2022-07-07 16:37:18 -0500

    Adding missing object-possessive form in she/her pronoun set

--- p2-Team-5/src/main/java/dev/stenope/models/Pronouns.java
+++ p2-Team-5/src/main/java/dev/stenope/models/Pronouns.java
@@ -6,7 +6,7 @@ public class Pronouns {
 			"e/em/eir/eirs/emself", //Spivak set
 			"ey/em/eir/eirs/emself", //Elverson set
 			"he/him/his/his/himself", //traditional masculine
-			"she/her/her/herself",  //traditional feminine
+			"she/her/her/hers/herself",  //traditional feminine
 			"they/them/thier/theirs/themself", //singular 'they'
 			"xe/xem/xyr/xyrs/xemself", // There's like 15 ways of spelling this one
 			"ze/zir/zir/zirs/zirself" // Ze/zir salad
