cbe9b6e2e2054ed22e10e59a7b25baff10fd74aa
wlcross <william589@revature.net>
2022-07-07 16:31:29 -0400

    Empty DAOs for Big 3 Types

---
+++ p2-Team-5/src/main/java/dev/stenope/respositories/ItemDAO.java
@@ -0,0 +1,13 @@
+package dev.stenope.respositories;
+
+import dev.stenope.models.Item;
+import dev.stenope.utils.ConnectionUtil;
+
+public class ItemDAO {
+	private static ConnectionUtil cu = ConnectionUtil.getConnectionUtil();
+
+	public boolean modifyItem(Item i) {
+		
+		return false;
+	}
+}
---
+++ p2-Team-5/src/main/java/dev/stenope/respositories/PetDAO.java
@@ -0,0 +1,30 @@
+package dev.stenope.respositories;
+
+import java.util.List;
+
+import dev.stenope.models.Pet;
+import dev.stenope.utils.ConnectionUtil;
+
+public class PetDAO {
+	private static ConnectionUtil cu = ConnectionUtil.getConnectionUtil();
+	
+	public Pet createPet(Pet p) {
+		
+		return null;
+	}
+	
+	public Pet modifyPet(Pet p) {
+		
+		return null;
+	}
+	
+	public Pet getPetByID(int id) {
+		
+		return null;
+	}
+	
+	public List<Pet> getPetListByUserID(int id) {
+		
+		return null;
+	}
+}
