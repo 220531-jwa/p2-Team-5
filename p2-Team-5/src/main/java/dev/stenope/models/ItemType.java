79eb6df0c0b946263d035257f0ac7e4a48d30f95
TCPrater <prater207@revature.net>
2022-07-07 16:10:16 -0500

    Adding more models

---
+++ p2-Team-5/src/main/java/dev/stenope/models/ItemType.java
@@ -0,0 +1,11 @@
+package dev.stenope.models;
+
+public class ItemType {
+	private int id; 
+	private int leftovers; 
+	private String tName; //type name (ex: pizza)
+	private String tCat; //type category (ex: food)
+	private String tSRC; //type image source
+	
+	
+}
---
+++ p2-Team-5/src/main/java/dev/stenope/models/PetTypes.java
@@ -0,0 +1,18 @@
+package dev.stenope.models;
+
+public class PetTypes {
+	public static String preloaded[][] = {
+			{"cockroach", "spider", "butterfly", "ladybug",
+			"earthworm", "octopus", "snail", "velvet worm", 
+			"dog", "cat", "rat", "horse", 
+			"owl", "t-rex", "frog", "fish"}, 
+			{"🪳", "🕷️", "🦋", "🐞",
+			"🪱", "🐙", "🐌", "🐛", 
+			"🐕", "🐈", "🐀", "🐎",
+			"🦉", "🦖", "🐸", "🐟"}
+			};
+	
+	private int id;
+	private String sName;
+	private String sSRC;
+}
