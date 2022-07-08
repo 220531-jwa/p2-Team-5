b9bd130f57a1a7a0b79a013e81b937d6c9f1ff4e
TCPrater <prater207@revature.net>
2022-07-08 14:03:12 -0500

    git is being snippy about overriding files

--- p2-Team-5/src/test/java/dev/stenope/steps/TopBarSteps.java
+++ p2-Team-5/src/test/java/dev/stenope/steps/TopBarSteps.java
@@ -1,5 +1,75 @@
 package dev.stenope.steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class TopBarSteps {
 
+	@Given("The User is logged in")
+	public void the_user_is_logged_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The User is on the Home Page")
+	public void the_user_is_on_the_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The User clicks on the User Profile")
+	public void the_user_clicks_on_the_user_profile() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The User navigates to the User Profile page")
+	public void the_user_navigates_to_the_user_profile_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The User clicks on the Inventory")
+	public void the_user_clicks_on_the_inventory() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The User navigates to the Inventory page")
+	public void the_user_navigates_to_the_inventory_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The User clicks on the Home Page")
+	public void the_user_clicks_on_the_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The User navigates to the Home Page page")
+	public void the_user_navigates_to_the_home_page_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The User is on the User Profile")
+	public void the_user_is_on_the_user_profile() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The User is on the Pet Profile")
+	public void the_user_is_on_the_pet_profile() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The User is on the Inventory")
+	public void the_user_is_on_the_inventory() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
 }
--- p2-Team-5/src/test/java/dev/stenope/steps/UserProfileSteps.java
+++ p2-Team-5/src/test/java/dev/stenope/steps/UserProfileSteps.java
@@ -1,5 +1,87 @@
 package dev.stenope.steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class UserProfileSteps {
 
+	@Given("a User Exists")
+	public void a_user_exists() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The User Page is loaded")
+	public void the_user_page_is_loaded() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The User display name is displayed")
+	public void the_user_display_name_is_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The User pronouns are displayed")
+	public void the_user_pronouns_are_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The User description is displayed")
+	public void the_user_description_is_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The User is on their own profile")
+	public void the_user_is_on_their_own_profile() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The User changes their display name")
+	public void the_user_changes_their_display_name() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The User presses submit")
+	public void the_user_presses_submit() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The User refreshes their profile")
+	public void the_user_refreshes_their_profile() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The User changes their pronouns")
+	public void the_user_changes_their_pronouns() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The User changes their description")
+	public void the_user_changes_their_description() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("a User Exists and has pets")
+	public void a_user_exists_and_has_pets() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("A list of the User's Pets is displayed")
+	public void a_list_of_the_user_s_pets_is_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
 }
