b49a4fd6d03a4882c5dfc949d81133fadb701724
joshuacoombs <h0wlpenwyrm@gmail.com>
2022-07-10 05:37:16 -0700

    adding further fixes to the forementioned, don't write tests before you understand them

--- p2-Team-5/src/test/java/dev/stenope/servicetests/PetServiceTest.java
+++ p2-Team-5/src/test/java/dev/stenope/servicetests/PetServiceTest.java
@@ -25,31 +25,30 @@ private static PetService petService;
 		mockPetDao = mock(PetDAO.class);
 		petService = new PetService(mockPetDao);
 	}
-	
 	@Test
 	public static void createPetTest() {
-		Pet testPet = new Pet(1, 1, 1, "Test", 0, 0, 0, 0, null);
+		Pet testPet = new Pet(1, 1, 1, "testName", "pronoun", 0, 0, 0, null);
 		when(mockPetDao.createPet(testPet)).thenReturn(testPet);
 		assertEquals(petService.createPet(testPet), testPet);
 	}
 	
 	@Test
 	public static void modifyPetTest() {
-		Pet testPet = new Pet(1, 1, 1, "Test", 0, 0, 0, 0, null);
+		Pet testPet = new Pet(1, 1, 1, "Test", "pronoun", 0, 0, 0, null);
 		when(mockPetDao.modifyPet(testPet)).thenReturn(true);
 		assertEquals(petService.modifyPet(testPet), true);
 	}
 	
 	@Test
 	public static void modifyPetInvalidTest() {
-		Pet testPet = new Pet(-1, 1, 1, "Test", 0, 0, 0, 0, null);
+		Pet testPet = new Pet(-1, 1, 1, "Test", "pronoun", 0, 0, 0, null);
 		when(mockPetDao.modifyPet(testPet)).thenReturn(false);
 		assertEquals(petService.modifyPet(testPet), false);
 	}
 	
 	@Test
 	public static void getPetTest() {
-		Pet testPet = new Pet(1, 1, 1, "Test", 0, 0, 0, 0, null);
+		Pet testPet = new Pet(1, 1, 1, "Test", "pronoun", 0, 0, 0, null);
 		when(mockPetDao.getPetByID(1)).thenReturn(testPet);
 		assertEquals(petService.getPetByID(1), testPet);
 	}
@@ -63,9 +62,9 @@ private static PetService petService;
 	@Test
 	public static void getPetListTest() {
 		ArrayList<Pet> testList = new ArrayList<Pet>();
-		testList.add(new Pet(1, 1, 1, "Test1", 0, 0, 0, 0, null));
-		testList.add(new Pet(2, 1, 1, "Test2", 0, 0, 0, 0, null));
-		testList.add(new Pet(3, 1, 1, "Test3", 0, 0, 0, 0, null));
+		testList.add(new Pet(1, 1, 1, "Test1", "pronoun", 0, 0, 0, null));
+		testList.add(new Pet(2, 1, 1, "Test2", "pronoun", 0, 0, 0, null));
+		testList.add(new Pet(3, 1, 1, "Test3", "pronoun", 0, 0, 0, null));
 		
 		when(mockPetDao.getPetListByUserID(1)).thenReturn(testList);
 		assertEquals(petService.getPetByID(1), testList);
