4d462baae2c851582f2171d2caea1b59a0bed49c
TCPrater <prater207@revature.net>
2022-07-11 14:18:39 -0500

    constructing pet profile runner

--- p2-Team-5/src/test/java/dev/stenope/runners/PetProfileRunner.java
+++ p2-Team-5/src/test/java/dev/stenope/runners/PetProfileRunner.java
@@ -1,5 +1,38 @@
 package dev.stenope.runners;
 
-public class PetProfileRunner {
+import java.io.File;
 
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import dev.stenope.pages.PetProfilePage;
+
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.BeforeAll;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith (Cucumber.class)
+@CucumberOptions(features = {"src/test/resources"}, glue = {"dev.stenope.steps"})
+public class PetProfileRunner {
+	public static WebDriver wd40;
+	public static PetProfilePage p3;
+	
+	@BeforeAll 
+	public static void setup()
+	{
+		File edge = new File("src/test/resources/msedgedriver.exe");
+		System.setProperty("webdriver.edge.driver", edge.getAbsolutePath());
+		
+		wd40 = new EdgeDriver(); 
+		
+		p3 = new PetProfilePage(wd40);
+		
+		System.out.println("inside setup method");
+	}
+	
+	@AfterAll
+	public static void teardown() {wd40.quit();}
+	
 }
