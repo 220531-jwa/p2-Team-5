ef16e391d173af25df47431900bcdb9b35568a03
TCPrater <prater207@revature.net>
2022-07-11 11:53:05 -0500

    pushing before lunch

--- p2-Team-5/src/main/resources/public/sténopé.js
+++ p2-Team-5/src/main/resources/public/sténopé.js
@@ -68,24 +68,26 @@ function loginCheck()
 //petPage
 function populatePetPage()
 {
+    populateTopBar();
     let contentment = ["feral", "dissociating", "bored", "neutral", "amused", "happy", "ecstatic"];
     let hunger = ["dying", "malnourished", "hungry", "neutral", "satisfied", "full", "bloated"]; 
     let owner = 0;
-    let pName = "";
+    let pName = null;
     let pSet = 0;
     let fun = 0; 
     let food = 0; 
     let level = 0;
-    let sName = "";
-    let sSRC = "";
+    let sName = null;
+    let sSRC = null;
 
     if (pName != null && sName != null) {document.getElementById("pNameBanner").innerText=`${pName} the ${sName}'s page!`;}
 
     if (pName != null && sName != null) {document.getElementById("pDataHere").innerHTML = `<h1>${sSRC}</h1><br>
-        <a id="ownerName" href="userPage/${owner}"></a>
+        <a id="ownerName" href="userPage/${owner}"></a><br>
         <label>Pet Name: <input id="petName" type="text" value="${pName}" readonly> the ${sName}</label><br>
         <label>Pronouns: 
-            <select id="petPSet" readonly>
+            <select id="petPSet" disabled>
+                <option value="0">${pronouns[0]}</option>
                 <option value="1">${pronouns[1]}</option>
                 <option value="2">${pronouns[2]}</option>
                 <option value="3">${pronouns[3]}</option>
@@ -94,8 +96,8 @@ function populatePetPage()
                 <option value="6">${pronouns[6]}</option>
             </select>
         </label><br>
-        <label>Contentment: <input id="funBox" type="number" value="${contentment[fun]}" readonly></label><br>
-        <label>Hunger: <input id="foodBox" type="number" value="${hunger[food]}" readonly></label><br>
+        <label>Contentment: <input id="funBox" type="text" value="${contentment[fun]}" readonly></label><br>
+        <label>Hunger: <input id="foodBox" type="text" value="${hunger[food]}" readonly></label><br>
         <label>Level: <input id="levelBox" type="number" value="${level}" readonly></label><br>`;}
 
     document.getElementById("ownerName").innerText = `User_${owner}`; //do something to get the owner's username here 
